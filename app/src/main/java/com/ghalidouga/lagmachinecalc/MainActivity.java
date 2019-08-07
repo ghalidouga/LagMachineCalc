a86ad8c6e5abb57efa528b1de5cdb7b6c983ccc5
ghalidouga <ghalighalidouga@gmail.com>
2019-08-08 00:07:26 +0700

    Initial commit

--- app/src/main/java/com/ghalidouga/lagmachinecalc/MainActivity.java
+++ app/src/main/java/com/ghalidouga/lagmachinecalc/MainActivity.java
@@ -22,15 +22,25 @@ public class MainActivity extends AppCompatActivity {
                 TextView resultPistonTextView = (TextView) findViewById(R.id.resultPistonTextView);
                 TextView resultObserverTextView = (TextView) findViewById(R.id.resultObserverTextView);
                 TextView resultArmorStandTextView = (TextView) findViewById(R.id.resulArmorStandTextView);
+                TextView resultCobbleStoneTextView = (TextView) findViewById(R.id.resultCobblestoneTextView);
+                TextView resultWoodPlankTextView = (TextView) findViewById(R.id.resultWoodPlankTextView);
+                TextView resultIronIngotTextView = (TextView) findViewById(R.id.resultIronIngotTextView);
 
                 int totalChunks = Integer.parseInt(totalChunkEditText.getText().toString());
                 int resultPiston = totalChunks * 256;
                 int resultObserver = totalChunks * 512;
                 int resultArmorStand = totalChunks * 256;
+                int resultCobbleStone = (resultPiston * 4) + ( resultObserver * 6);
+                int resultWoodPlank = resultPiston * 3;
+                int resultIronIngot = resultPiston * 1;
+
 
                 resultPistonTextView.setText(resultPiston + "");
                 resultObserverTextView.setText(resultObserver + "");
                 resultArmorStandTextView.setText(resultArmorStand + "");
+                resultCobbleStoneTextView.setText(resultCobbleStone + "");
+                resultWoodPlankTextView.setText(resultWoodPlank + "");
+                resultIronIngotTextView.setText(resultIronIngot + "");
             }
         });
 //
