7028a8f79819b906e7994b663223b5870b831c4e
ghalidouga <ghalighalidouga@gmail.com>
2019-08-08 21:02:11 +0700

    adding a view per stack

--- app/src/main/java/com/ghalidouga/lagmachinecalc/MainActivity.java
+++ app/src/main/java/com/ghalidouga/lagmachinecalc/MainActivity.java
@@ -3,6 +3,7 @@ package com.ghalidouga.lagmachinecalc;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,13 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_main);
         Button confirmButton = (Button) findViewById(R.id.confirmButton);
+
+
         if (getIntent().hasExtra("com.ghalidouga.lagmachinecalc.START")){
             EditText totalChunkEditText = (EditText) findViewById(R.id.totalChunkEditText);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.viewPerStackCheckBox);
             totalChunkEditText.setText("0");
         }
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                CheckBox checkBox = (CheckBox) findViewById(R.id.viewPerStackCheckBox);
                 EditText totalChunkEditText = (EditText) findViewById(R.id.totalChunkEditText);
                 TextView resultPistonTextView = (TextView) findViewById(R.id.resultPistonTextView);
                 TextView resultObserverTextView = (TextView) findViewById(R.id.resultObserverTextView);
@@ -34,30 +39,56 @@ public class MainActivity extends AppCompatActivity {
                 TextView resultStickTextView = (TextView) findViewById(R.id.resultStickTextView);
                 TextView resultStoneSlabTextView = (TextView) findViewById(R.id.resultStoneSlabTextView);
 
-                int totalChunks = Integer.parseInt(totalChunkEditText.getText().toString());
-                int resultPiston = totalChunks * 256;
-                int resultObserver = totalChunks * 512;
-                int resultArmorStand = totalChunks * 256;
-                int resultCobbleStone = (resultPiston * 4) + ( resultObserver * 6);
-                int resultWoodPlank = resultPiston * 3;
-                int resultIronIngot = resultPiston;
-                int resultRedstoneDust = (resultPiston) + (resultObserver * 2);
-                int resultNetherQuartz = resultObserver;
-                int resultStick = resultArmorStand * 6;
-                int resultStoneSlab = resultArmorStand;
+                if(checkBox.isChecked()) {
+                    int totalChunks = Integer.parseInt(totalChunkEditText.getText().toString());
+                    int resultPiston = (totalChunks * 256) / 64;
+                    int resultObserver = totalChunks * 512 / 64;
+                    int resultArmorStand = totalChunks * 256 / 16;
+                    int resultCobbleStone = (resultPiston * 4) + (resultObserver * 6);
+                    int resultWoodPlank = (resultPiston * 3);
+                    int resultIronIngot = resultPiston;
+                    int resultRedstoneDust = ((resultPiston) + (resultObserver * 2));
+                    int resultNetherQuartz = resultObserver;
+                    int resultStick = resultArmorStand * 6 / 4;
+                    int resultStoneSlab = resultArmorStand / 4;
 
 
+                    resultPistonTextView.setText(resultPiston + " Stack");
+                    resultObserverTextView.setText(resultObserver + " Stack");
+                    resultArmorStandTextView.setText(resultArmorStand + " Stack");
+                    resultCobbleStoneTextView.setText(resultCobbleStone + " Stack");
+                    resultWoodPlankTextView.setText(resultWoodPlank + " Stack");
+                    resultIronIngotTextView.setText(resultIronIngot + " Stack");
+                    resultRedstoneDustTextView.setText(resultRedstoneDust + " Stack");
+                    resultNetherQuartzTextView.setText(resultNetherQuartz + " Stack");
+                    resultStickTextView.setText(resultStick + " Stack");
+                    resultStoneSlabTextView.setText(resultStoneSlab + " Stack");
+                } else{
+                    int totalChunks = Integer.parseInt(totalChunkEditText.getText().toString());
+                    int resultPiston = totalChunks * 256;
+                    int resultObserver = totalChunks * 512;
+                    int resultArmorStand = totalChunks * 256;
+                    int resultCobbleStone = (resultPiston * 4) + (resultObserver * 6);
+                    int resultWoodPlank = resultPiston * 3;
+                    int resultIronIngot = resultPiston;
+                    int resultRedstoneDust = (resultPiston) + (resultObserver * 2);
+                    int resultNetherQuartz = resultObserver;
+                    int resultStick = resultArmorStand * 6;
+                    int resultStoneSlab = resultArmorStand;
 
-                resultPistonTextView.setText(resultPiston + "");
-                resultObserverTextView.setText(resultObserver + "");
-                resultArmorStandTextView.setText(resultArmorStand + "");
-                resultCobbleStoneTextView.setText(resultCobbleStone + "");
-                resultWoodPlankTextView.setText(resultWoodPlank + "");
-                resultIronIngotTextView.setText(resultIronIngot + "");
-                resultRedstoneDustTextView.setText(resultRedstoneDust + "");
-                resultNetherQuartzTextView.setText(resultNetherQuartz + "");
-                resultStickTextView.setText(resultStick + "");
-                resultStoneSlabTextView.setText(resultStoneSlab + "");
+
+                    resultPistonTextView.setText(resultPiston + "");
+                    resultObserverTextView.setText(resultObserver + "");
+                    resultArmorStandTextView.setText(resultArmorStand + "");
+                    resultCobbleStoneTextView.setText(resultCobbleStone + "");
+                    resultWoodPlankTextView.setText(resultWoodPlank + "");
+                    resultIronIngotTextView.setText(resultIronIngot + "");
+                    resultRedstoneDustTextView.setText(resultRedstoneDust + "");
+                    resultNetherQuartzTextView.setText(resultNetherQuartz + "");
+                    resultStickTextView.setText(resultStick + "");
+                    resultStoneSlabTextView.setText(resultStoneSlab + "");
+                }
+
             }
         });
 //
